98231376ed3d7f822ad44210248673c84f56b7d9
Ism1tha <xgalan@hotmail.com>
2025-01-28 12:56:23 +0000

    fix: separate Vite asset imports for better clarity

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,7 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ config('app.name') }}</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}" />
-    @vite(['resources/ts/main.tsx', 'resources/css/app.css'])
+    @vite('resources/ts/main.tsx')
+    @vite('resources/css/app.css')
 </head>
 
 <body>
