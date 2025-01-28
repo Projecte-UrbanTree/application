f3f6771202b12dfe7e6b62c6fd11b0f3f8bbf5f9
Ism1tha <xgalan@hotmail.com>
2025-01-28 11:29:20 +0000

    feat: add Prettier configuration and integrate with ESLint for improved code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -12,4 +13,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginPrettier,
+  configPrettier,
+  { rules: { "prettier/prettier": "error" } },
 ];
