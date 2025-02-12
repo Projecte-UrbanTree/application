b8c99993a9a229081cfecd8c8a405eced8148f51
Ism1tha <xgalan@hotmail.com>
2025-02-12 17:04:12 +0100

    feat(admin): replace welcome icon with a custom icon from Iconify in Welcome component

--- resources/ts/components/Admin/Dashboard/Welcome.tsx
+++ resources/ts/components/Admin/Dashboard/Welcome.tsx
@@ -1,6 +1,7 @@
 import { useI18n } from '@/hooks/useI18n';
-
 import { useAuth } from '@/hooks/useAuth';
+
+import { Icon } from '@iconify/react';
 
 export function Welcome() {
   const { user } = useAuth();
@@ -8,7 +9,9 @@ export function Welcome() {
   return (
     <div className="bg-gray-50 rounded p-6 mb-8 border border-gray-300">
       <div className="flex items-center space-x-4">
-        <span className="text-4xl text-gray-800">👋</span>
+        <span className="text-4xl text-gray-800">
+          <Icon icon="tabler:trees" color="#8BCC63" />
+        </span>
         <div>
           <h1 className="text-2xl font-semibold text-gray-800">
             {t('admin.pages.dashboard.welcome.greeting', {
