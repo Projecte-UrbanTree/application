2e89649c4bcc650d79ab0d954a21b56351093db0
Ism1tha <xgalan@hotmail.com>
2025-02-12 14:03:48 +0100

    feat(auth): enhance authentication hook and layout with language selector

--- resources/ts/layouts/AuthLayout.tsx
+++ resources/ts/layouts/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from '@resources/images/logo.png';
+import LangSelector from '@/components/LangSelector';
+import logo from '@images/logo.png';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
@@ -13,12 +14,13 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children, title }) => {
 
   return (
     <div className="font-sans leading-normal tracking-normal flex items-center justify-center h-screen p-2 md:p-0">
-      <div className="w-full max-w-lg bg-white rounded p-8 border border-gray-200">
-        {/* Header with Logo */}
+      <div className="w-full max-w-xl bg-white rounded p-8 border border-gray-200">
+        <div className="flex justify-end">
+          <LangSelector />
+        </div>
         <header className="text-center my-8">
           <img src={logo} alt="Logo" className="mx-auto mb-4 w-48 md:w-64" />
         </header>
-        {/* Main content */}
         <main>{children}</main>
       </div>
     </div>
