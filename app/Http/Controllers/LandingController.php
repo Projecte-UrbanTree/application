2cc989828b29caf437d24d3fa64a106d4edf325a
Ism1tha <xgalan@hotmail.com>
2025-02-13 16:16:38 +0100

    feat(landing): add index method to return landing view
    Co-authored-by: 24Victor <118720584+24Victor@users.noreply.github.com>

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
+    public function index()
+    {
+        return view('landing');
+    }
+
     public function store(Request $request)
     {
         $request->validate([
