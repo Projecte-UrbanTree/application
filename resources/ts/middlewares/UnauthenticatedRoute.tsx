85e6b5544b92253bccb746ee85cc7ba2e38ded67
Ism1tha <xgalan@hotmail.com>
2025-02-13 07:46:41 +0100

    feat(middlewares): enhance UnauthenticatedRoute to redirect users based on roles

--- resources/ts/middlewares/UnauthenticatedRoute.tsx
+++ resources/ts/middlewares/UnauthenticatedRoute.tsx
@@ -2,7 +2,15 @@ import { Navigate, Outlet } from 'react-router';
 import { useAuthContext } from '@/contexts/AuthContext';
 
 export default function UnauthenticatedRoute() {
-  const { isAuthenticated } = useAuthContext();
+  const { isAuthenticated, user } = useAuthContext();
 
-  return !isAuthenticated ? <Outlet /> : <Navigate to="/admin/dashboard" />;
+  return !isAuthenticated ? (
+    <Outlet />
+  ) : user?.role === 'admin' ? (
+    <Navigate to="/admin/dashboard" replace />
+  ) : user?.role === 'worker' ? (
+    <Navigate to="/worker" replace />
+  ) : (
+    <Navigate to="/customer" replace />
+  );
 }
