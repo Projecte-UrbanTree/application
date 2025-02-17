f95669f00c4e7fd0092d4500d925bd99c1f9b68e
0x1026 <69076992+0x1026@users.noreply.github.com>
2025-02-17 11:55:51 +0100

    refactor: make landing routes dynamic and update form handling

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -7,27 +7,36 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
-    public function index()
+    public function index(int $number)
     {
-        return view('/Landings/landing11');
+        return view("/Landings/landing$number");
     }
 
-    public function store(Request $request)
+    public function store(int $number, Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:150',
-            'email' => 'required|email|max:255',
-            'phone' => 'nullable|regex:/^(?=(?:\D*\d){9}\D*$)[0-9\s]+$/',
-            'message' => 'required|string|min:10|max:255',
-        ]);
+        switch ($number) {
+            case 11:
+                $request->validate([
+                    'name' => 'required|string|max:150',
+                    'email' => 'required|email|max:255',
+                    'phone' => 'nullable|regex:/^(?=(?:\D*\d){9}\D*$)[0-9\s]+$/',
+                    'message' => 'required|string|min:10|max:255',
+                ]);
 
-        $landingForm = new LandingForm;
-        $landingForm->name = $request->name;
-        $landingForm->email = $request->email;
-        $landingForm->phone = $request->phone;
-        $landingForm->message = $request->message;
-        $landingForm->save();
+                $landingForm = new LandingForm;
+                $landingForm->name = $request->name;
+                $landingForm->email = $request->email;
+                $landingForm->phone = $request->phone;
+                $landingForm->message = $request->message;
+                $landingForm->save();
 
-        return redirect('/landing11')->with('success', 'Mensaje enviado correctamente.');
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
+        return redirect("/landing$number")->with('success', 'Mensaje enviado correctamente.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\LandingController;
 use App\Http\Controllers\LangController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/landing11', [LandingController::class, 'index'])->name('landing');
+Route::get('/landing{number}', [LandingController::class, 'index'])->where('number', '[0-9]+')->name('landing');
 
-Route::post('/landing11/landing11-form', [LandingController::class, 'store'])->name('landing.form');
+Route::post('/landing{number}/form', [LandingController::class, 'store'])->name('landing.form');
 
 Route::get('/set-language/{lang}', [LangController::class, 'setLanguage'])->name('set-language');
 
