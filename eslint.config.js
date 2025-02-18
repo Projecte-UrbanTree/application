9e37b4e621c6fde3af2da20d160f69ff1dee9edf
Ism1tha <xgalan@hotmail.com>
2025-02-18 13:14:44 +0100

    refactor: update ESLint configuration and dependencies for improved compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import pluginPrettier from "eslint-plugin-prettier";
-import configPrettier from "eslint-config-prettier";
+// @ts-check
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {settings: {react: {version: "detect"}, "import/resolver": { "typescript": {} }}},
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginPrettier,
-  configPrettier,
-  { rules: { "prettier/prettier": "error" } },
-];
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettier,
+);
