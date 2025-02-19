31fef799163bc5de15d14d87e8777562664652a3
Chuck.D.Norris <boveoriol@gmail.com>
2025-02-19 15:16:30 +0100

    landing1-change

--- resources/views/landings/landing1.blade.php
+++ resources/views/landings/landing1.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ __('landings/landing1.title') }}</title>
-    <script src="https://kit.fontawesome.com/YOUR_KIT.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    
 </head>
 
 <body class="bg-gray-100 text-gray-900">
