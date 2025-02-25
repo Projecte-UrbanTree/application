03bd70680d68f42ff9e847640f26baa3a0925921
Oriol Reverte <oreverte@avanzaprl.es>
2025-02-25 17:49:36 +0100

    feat: implement user authentication with Redux and create user slice

--- resources/ts/hooks/useAuth.ts
+++ resources/ts/hooks/useAuth.ts
@@ -1,35 +1,48 @@
 import { useEffect } from 'react';
-import { useAuthContext } from '@/contexts/AuthContext';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/contexts/store/store';
+import { PassThrough } from 'stream';
+import { UserData } from '@/types/user';
+import { log } from 'console';
+import { clearUserData, setUserData } from '@/contexts/store/slice/userSlice';
+import axios from 'axios';
 
 export function useAuth() {
-  const {
-    isLoading,
-    isAuthenticated,
-    signIn,
-    signOut,
-    user,
-    setUser,
-    setIsLoading,
-  } = useAuthContext();
+    const dispatch = useDispatch();
+    const user = useSelector((state: RootState) => state.user);
+    const isAuthenticated = Boolean(user.id);
+    const isLoading = !user.id && localStorage.getItem('authToken') !== null;
 
-  useEffect(() => {
-    const token = localStorage.getItem('authToken');
-    if (token && !isAuthenticated) {
-      signIn();
-    }
-  }, [isAuthenticated, signIn]);
+    useEffect(() => {
+        const token = localStorage.getItem('authToken');
+        if (token && !user.id) {
+            fetchUser();
+        }
+    }, [user.id]);
 
-  const login = (token: string, user: any) => {
-    localStorage.setItem('authToken', token);
-    signIn();
-    setUser(user);
-  };
+    const fetchUser = async () => {
+        try {
+            const response = await axios.get('/user');
+            console.log('RESPONSE fetchUser: ', { response });
 
-  const logout = () => {
-    localStorage.removeItem('authToken');
-    signOut();
-    setUser(null);
-  };
+            dispatch(setUserData(response.data));
+        } catch (error) {
+            console.error(error);
+            logout();
+        }
+    };
 
-  return { isLoading, isAuthenticated, user, login, logout, setIsLoading };
+    const login = (token: string, userData: UserData) => {
+        if (userData !== null) {
+            console.log('USERDATA: ', { token, userData });
+            dispatch(setUserData(userData));
+        }
+    };
+
+    const logout = () => {
+        console.log('LOGOUT');
+        dispatch(clearUserData());
+    };
+
+    return { isLoading, isAuthenticated, user, login, logout: logout };
 }
--- resources/ts/main.tsx
+++ resources/ts/main.tsx
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { PrimeReactProvider } from 'primereact/api';
 import '@resources/css/app.css';
+import { Provider } from 'react-redux';
 
 import '@/config/i18n';
-import { AuthProvider } from '@/contexts/AuthContext';
 import { ToastProvider } from '@/contexts/ToastContext';
+import store from '@/contexts/store/store';
 
 const rootElement = document.getElementById('app');
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement); // This is correct for React 18+
-  root.render(
-    <React.StrictMode>
-      <PrimeReactProvider>
-        <AuthProvider>
-          <ToastProvider>
-            <App />
-          </ToastProvider>
-        </AuthProvider>
-      </PrimeReactProvider>
-    </React.StrictMode>,
-  );
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <PrimeReactProvider>
+                <Provider store={store}>
+                    <ToastProvider>
+                        <App />
+                    </ToastProvider>
+                </Provider>
+            </PrimeReactProvider>
+        </React.StrictMode>,
+    );
 }
