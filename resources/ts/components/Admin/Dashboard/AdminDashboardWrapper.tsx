86f95cf5c3225a48d4a682affce7871b0456cdcc
Oriol Reverte <oreverte@avanzaprl.es>
2025-02-26 10:32:43 +0100

    feat: initialize contract state in AdminDashboardWrapper with mock data

--- resources/ts/components/Admin/Dashboard/AdminDashboardWrapper.tsx
+++ resources/ts/components/Admin/Dashboard/AdminDashboardWrapper.tsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/contexts/store/store';
 import { ContractProps } from '@/types/contract';
 import AdminLayout from '@/layouts/AdminLayout';
+import { setContractState } from '@/contexts/store/slice/contractSlice';
+import { useEffect } from 'react';
 
 interface AdminLayoutWrapperProps {
     titleI18n: string;
@@ -14,12 +16,34 @@ export default function AdminLayoutWrapper({
 }: AdminLayoutWrapperProps) {
     const dispatch = useDispatch();
 
+    const mockContracts = [
+        { id: '1', name: 'Tortosa' },
+        { id: '2', name: 'Barcelona' },
+        { id: '3', name: 'Valencia' },
+        { id: '4', name: 'Madrid' },
+        { id: '5', name: 'Sevilla' },
+    ];
+
+    useEffect(() => {
+        dispatch(
+            setContractState({
+                allContracts: mockContracts,
+                currentContract: mockContracts[0],
+            }),
+        );
+    }, [dispatch]);
+
     const contracts = useSelector(
         (state: RootState) => state.contract.allContracts,
     );
+
+    console.log('All contrats: ', contracts);
+
     const currentContract = useSelector(
         (state: RootState) => state.contract.currentContract,
     );
+
+    console.log('Current Contract: ', currentContract);
 
     // TODO: MAKE THE API CALL FOR OBTAIN ALL THE CONTRACTS
     // TODO: SAVE THE DATA FROM RESPONSE TO => dispatch(setContractState( {...} ))
