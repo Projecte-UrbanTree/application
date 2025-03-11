9abcfa17983cc1185fa73b97b0f056f11f30efba
Chuck.D.Norris <boveoriol@gmail.com>
2025-03-11 16:05:23 +0100

    test: implement more admin tests

--- tests/Unit/AdminTest.php
+++ tests/Unit/AdminTest.php
@@ -7,6 +7,7 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\ResourceType;
 use App\Models\ElementType;
+use App\Models\TaskType;
 
 class AdminTest extends TestCase
 {
@@ -77,15 +78,92 @@ class AdminTest extends TestCase
         ]);
     }
 
-    public function test_user_cannot_create_element_type_with_symbols()
+    public function test_user_cannot_create_resource_type_with_symbols()
+        {
+            $this->actingAs($this->user);
+
+            $response = $this->post('/resource-types', [
+                'name' => 'Resource@Type#1!',
+                'description' => null,
+            ]);
+
+            $response->assertSessionHasErrors('name');
+        }
+
+
+    public function test_user_can_create_task_type()
     {
         $this->actingAs($this->user);
+
+        $taskType = TaskType::create([
+            'name' => 'TaskType1',
+            'description' => null,
+        ]);
+
+        $this->assertDatabaseHas('task_types', [
+            'id' => $taskType->id,
+            'name' => 'TaskType1',
+            'description' => null,
+        ]);
+    }
+
+    public function test_user_can_edit_existing_user()
+    {
+        $this->actingAs($this->user);
+
+        $existingUser = User::factory()->create([
+            'name' => 'Old Name',
+            'surname' => 'Old Surname'
+        ]);
+
+        $existingUser->update([
+            'name' => 'New Name',
+            'surname' => 'New Surname'
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $existingUser->id,
+            'name' => 'New Name',
+            'surname' => 'New Surname'
+        ]);
+    }
+
+    public function test_user_can_delete_existing_user()
+    {
+        $this->actingAs($this->user);
+
+        $existingUser = User::factory()->create([
+            'name' => 'User To Delete',
+            'surname' => 'Surname To Delete'
+        ]);
+
+        $existingUser->delete();
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $existingUser->id
+        ]);
+    }
+
+    public function test_inputs_have_validation_to_deny_symbols()
+    {
+        $this->actingAs($this->user);
+
+        $response = $this->post('/resource-types', [
+            'name' => 'Resource@Type#1!',
+            'description' => null,
+        ]);
+        $response->assertSessionHasErrors('name');
 
         $response = $this->post('/element-types', [
             'name' => 'Element@Type#1!',
             'description' => null,
         ]);
+        $response->assertSessionHasErrors('name');
 
+        $response = $this->post('/task-types', [
+            'name' => 'Task@Type#1!',
+            'description' => null,
+        ]);
         $response->assertSessionHasErrors('name');
     }
 }
