314aff84dcd51804d03024413bc688877fc93645
Bot Urban Tree 5.0 <urbantree@iesmontsia.org>
2025-03-11 18:42:01 +0100

    New translations passwords.php (Turkish)

---
+++ lang/tr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'crwdns2615:0crwdne2615:0',
+    'sent' => 'crwdns2617:0crwdne2617:0',
+    'token' => 'crwdns2619:0crwdne2619:0',
+    'user' => 'crwdns2621:0crwdne2621:0',
+    'throttled' => 'crwdns2623:0crwdne2623:0',
+    'password' => 'crwdns2625:0crwdne2625:0',
+];
