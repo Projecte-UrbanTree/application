b8916c160b83b90e136ebcb651fe9d288f351564
Bot Urban Tree 5.0 <urbantree@iesmontsia.org>
2025-03-11 18:41:56 +0100

    New translations auth.php (Turkish)

---
+++ lang/tr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'crwdns2607:0crwdne2607:0',
+    'throttle' => 'crwdns2609:0crwdne2609:0',
+
+];
