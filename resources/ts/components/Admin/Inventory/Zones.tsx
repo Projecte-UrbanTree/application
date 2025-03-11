580fa465f1f0144c19b442307e520533feb7ebe8
Oriol Reverte <oreverte@avanzaprl.es>
2025-03-11 20:51:05 +0100

    ``` feat: implement new feature for user authentication and session management ```

--- resources/ts/components/Admin/Inventory/Zones.tsx
+++ resources/ts/components/Admin/Inventory/Zones.tsx
@@ -1,6 +1,21 @@
+import axiosClient from '@/api/axiosClient';
+import { Zone } from '@/types/zone';
 import { Button } from 'primereact/button';
+import { useEffect, useState } from 'react';
 
 export const Zones = () => {
+    const [zones, setZones] = useState<Zone[]>([]);
+
+    useEffect(() => {
+        const fetchZones = async () => {
+            const response = await axiosClient.get<Zone[]>(`/admin/zones`);
+            const zones: Zone[] = response.data;
+            setZones(zones);
+        };
+
+        fetchZones();
+    }, [zones]);
+
     return (
         <div>
             <Button label="test" />
