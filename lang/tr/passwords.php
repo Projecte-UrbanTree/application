2d7f9f385a3be7629f5b8caba1b63e9b22a3237e
Bot Urban Tree 5.0 <urbantree@iesmontsia.org>
2025-03-12 10:39:39 +0100

    New translations passwords.php (Turkish)

--- lang/tr/passwords.php
+++ lang/tr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'crwdns1938:0crwdne1938:0',
-    'sent' => 'crwdns1940:0crwdne1940:0',
-    'token' => 'crwdns1942:0crwdne1942:0',
-    'user' => 'crwdns1944:0crwdne1944:0',
-    'throttled' => 'crwdns1946:0crwdne1946:0',
-    'password' => 'crwdns1948:0crwdne1948:0',
+    'reset' => 'crwdns2615:0crwdne2615:0',
+    'sent' => 'crwdns2617:0crwdne2617:0',
+    'token' => 'crwdns2619:0crwdne2619:0',
+    'user' => 'crwdns2621:0crwdne2621:0',
+    'throttled' => 'crwdns2623:0crwdne2623:0',
+    'password' => 'crwdns2625:0crwdne2625:0',
 ];
