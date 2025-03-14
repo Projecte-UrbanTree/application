ee79c2b4bd17a5c13df3f07eaa1b16db2207f656
0x1026 <69076992+0x1026@users.noreply.github.com>
2025-03-14 00:14:11 +0000

    style: format using Laravel Pint (via GitHub Actions)

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 return [
-    'password'  => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.',
-    'reset'     => 'Your password has been reset.',
-    'sent'      => 'We have emailed your password reset link.',
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.',
+    'reset' => 'Your password has been reset.',
+    'sent' => 'We have emailed your password reset link.',
     'throttled' => 'Please wait before retrying.',
-    'token'     => 'This password reset token is invalid.',
-    'user'      => 'We can\'t find a user with that email address.',
+    'token' => 'This password reset token is invalid.',
+    'user' => 'We can\'t find a user with that email address.',
 ];
