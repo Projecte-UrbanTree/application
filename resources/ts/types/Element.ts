1ced751e3093322b6e85fa140779a53362578c05
oriolreverte36 <oreverte@avanzaprl.es>
2025-03-16 17:33:09 +0100

    feat: add Element interface to define structure for element data

---
+++ resources/ts/types/Element.ts
@@ -0,0 +1,9 @@
+export interface Element {
+  id: number;
+  description: string;
+  element_type_id: number;
+  tree_type_id: number;
+  point_id: number;
+  created_at: null;
+  updated_at: null;
+}
