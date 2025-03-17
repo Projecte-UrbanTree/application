966eca062779eec73291b02dbf603a4588ea8764
Oriol Reverte <oreverte@avanzaprl.es>
2025-03-17 17:52:23 +0100

    feat: enhance Zones component to display a message when no zones are available and streamline zone handling

--- resources/ts/components/Admin/Inventory/Zones.tsx
+++ resources/ts/components/Admin/Inventory/Zones.tsx
@@ -98,48 +98,55 @@ export const Zones = ({ onSelectedZone, onAddElementZone }: ZoneProps) => {
 
   return (
     <div className="p-4 h-full overflow-y-auto bg-transparent rounded-lg shadow-md">
-      <Accordion multiple activeIndex={null} className="w-full">
-        {uniqueZones.map((zone: Zone) => (
-          <AccordionTab
-            key={zone.id}
-            header={
-              <div className="flex justify-between items-center w-full">
-                <div className="flex items-center gap-2">
-                  <div
-                    className="w-4 h-4 rounded-full"
-                    style={{
-                      backgroundColor: zone.color || 'gray',
-                    }}></div>
-                  <span className="text-sm font-medium">{zone.name}</span>
+      {uniqueZones.length > 0 ? (
+        <Accordion multiple activeIndex={null} className="w-full">
+          {uniqueZones.map((zone: Zone) => (
+            <AccordionTab
+              key={zone.id}
+              header={
+                <div className="flex justify-between items-center w-full">
+                  <div className="flex items-center gap-2">
+                    <div
+                      className="w-4 h-4 rounded-full"
+                      style={{
+                        backgroundColor: zone.color || 'gray',
+                      }}></div>
+                    <span className="text-sm font-medium">{zone.name}</span>
+                  </div>
+                  <Button
+                    icon={<Icon icon="mdi:map-marker" width="20" />}
+                    className="p-button-text p-2"
+                    onClick={() => onSelectedZone(zone)}
+                  />
                 </div>
+              }>
+              <div className="p-2 text-sm text-gray-700 flex justify-between items-center">
+                <p>
+                  <strong>Descripción:</strong> {zone.description}
+                </p>
                 <Button
-                  icon={<Icon icon="mdi:map-marker" width="20" />}
-                  className="p-button-text p-2"
-                  onClick={() => onSelectedZone(zone)}
+                  icon={<Icon icon="mdi:trash-can-outline" width="20" />}
+                  className="p-button-danger p-button-text p-2"
+                  onClick={() => confirmDeleteZone(zone)}
                 />
               </div>
-            }>
-            <div className="p-2 text-sm text-gray-700 flex justify-between items-center">
-              <p>
-                <strong>Descripción:</strong> {zone.description}
-              </p>
-              <Button
-                icon={<Icon icon="mdi:trash-can-outline" width="20" />}
-                className="p-button-danger p-button-text p-2"
-                onClick={() => confirmDeleteZone(zone)}
-              />
-            </div>
-            <div className="p-2 text-sm text-gray-700 flex justify-between items-center">
-              <strong>Añadir elemento</strong>
-              <Button
-                className="p-button p-button-text p-2"
-                icon={<Icon icon="mdi:add" />}
-                onClick={() => addElementZone(zone)}
-              />
-            </div>
-          </AccordionTab>
-        ))}
-      </Accordion>
+              <div className="p-2 text-sm text-gray-700 flex justify-between items-center">
+                <strong>Añadir elemento</strong>
+                <Button
+                  className="p-button p-button-text p-2"
+                  icon={<Icon icon="mdi:add" />}
+                  onClick={() => addElementZone(zone)}
+                />
+              </div>
+            </AccordionTab>
+          ))}
+        </Accordion>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-full text-gray-500 text-lg">
+          <Icon icon="mdi:alert-circle-outline" width="32" className="mb-2" />
+          <p>No hay zonas en este contrato</p>
+        </div>
+      )}
 
       <Dialog
         header="Confirmar eliminación"
