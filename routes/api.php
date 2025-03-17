dee6d1e3caef1daf079c2e617750943728f419f1
0x1026 <69076992+0x1026@users.noreply.github.com>
2025-03-17 17:17:47 +0100

    refactor: clean up AccountController and improve route organization

--- routes/api.php
+++ routes/api.php
@@ -12,23 +12,24 @@ use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\Admin\WorkOrderController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Middleware\RoleMiddleware;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Models\Contract;
 use App\Models\Element;
 use App\Models\User;
 use App\Models\WorkOrder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
+    /* Generic protected routes */
     Route::get('user', function (Request $request) {
         return $request->user();
     });
-
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::middleware(RoleMiddleware::class.':admin')->prefix('admin')->group(function () {
+    /* Admin protected routes */
+    Route::middleware(RoleMiddleware::class . ':admin')->prefix('admin')->group(function () {
         Route::post('select-contract', [ContractController::class, 'selectContract']);
         Route::get('get-selected-contract', [ContractController::class, 'getSelectedContract']);
 
@@ -44,36 +45,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('account', [AccountController::class, 'show']);
         Route::put('account', [AccountController::class, 'update']);
         Route::put('account/password', [AccountController::class, 'updatePassword']);
-
-        Route::get('contracts', [ContractController::class, 'index']);
-        Route::post('contracts', [ContractController::class, 'store']);
-        Route::get('contracts/{id}', [ContractController::class, 'show']);
-        Route::put('contracts/{id}', [ContractController::class, 'update']);
-        Route::delete('contracts/{id}', [ContractController::class, 'destroy']);
-
-        Route::get('work-orders', [WorkOrderController::class, 'index']);
-        Route::get('element-types', [ElementTypeController::class, 'index']);
-
-        Route::get('tree-types', [TreeTypeController::class, 'index']);
-        Route::post('tree-types', [TreeTypeController::class, 'store']);
-        Route::get('tree-types/{id}', [TreeTypeController::class, 'show']);
-        Route::put('tree-types/{id}', [TreeTypeController::class, 'update']);
-        Route::delete('tree-types/{id}', [TreeTypeController::class, 'destroy']);
-
-        Route::get('task-types', [TaskTypeController::class, 'index']);
-        Route::post('task-types', [TaskTypeController::class, 'store']);
-        Route::get('task-types/{id}', [TaskTypeController::class, 'show']);
-        Route::put('task-types/{id}', [TaskTypeController::class, 'update']);
-        Route::delete('task-types/{id}', [TaskTypeController::class, 'destroy']);
-
-        Route::get('resources', [ResourceController::class, 'index']);
-        Route::get('resource-types', [ResourceTypeController::class, 'index']);
-
-        Route::get('users', [UserController::class, 'index']);
-        Route::post('users', [UserController::class, 'store']);
-        Route::get('users/{id}', [UserController::class, 'show']);
-        Route::put('users/{id}', [UserController::class, 'update']);
-        Route::delete('users/{id}', [UserController::class, 'destroy']);
         // Route for stats
         Route::get('statistics', [StatisticsController::class, 'index']);
 
