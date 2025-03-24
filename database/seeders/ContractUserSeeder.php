ce6cb045d2271a07183e49d61d065b7618e303ee
0x1026 <69076992+0x1026@users.noreply.github.com>
2025-03-24 16:53:11 +0100

    feat(contract): add ContractUserController and pivot table for user assignments

---
+++ database/seeders/ContractUserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContractUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('contract_user')->insert([
+            [
+                'user_id' => 2,
+                'contract_id' => 1,
+            ],
+            [
+                'user_id' => 3,
+                'contract_id' => 1,
+            ],
+            [
+                'user_id' => 5,
+                'contract_id' => 2,
+            ],
+            [
+                'user_id' => 3,
+                'contract_id' => 3,
+            ],
+        ]);
+    }
+}
