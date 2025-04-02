be4d97bcb5e8167034c7543a1624cd96ad0a10e9
Chuck.D.Norris <boveoriol@gmail.com>
2025-04-02 21:09:59 +0200

    feat/(seed): create a zone point

--- database/seeders/PointSeeder.php
+++ database/seeders/PointSeeder.php
@@ -19,6 +19,29 @@ class PointSeeder extends Seeder
                 'type' => 'zone_delimiter',
                 'zone_id' => 1,
             ],
+            
+            [
+                'latitude' => 40.70582831162543,
+                'longitude' => 0.5844847646902142,
+                'type' => 'zone_delimiter',
+                'zone_id' => 1,
+            ],
+
+            [
+                'latitude' => 40.70365351307243, 
+                'longitude' => 0.5843657681033299,
+                'type' => 'zone_delimiter',
+                'zone_id' => 1,
+            ],
+
+            [
+                'latitude' => 40.70368696361823,  
+                'longitude' => 0.5819663392783148,
+                'type' => 'zone_delimiter',
+                'zone_id' => 1,
+            ],
+            
+            
             [
                 'latitude' => 40.70866070784594,
                 'longitude' =>  0.5779260143620888,
