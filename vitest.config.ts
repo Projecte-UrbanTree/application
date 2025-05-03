e935f040e6c290a55615e12b14268ed84ba7f331
CODEmartigarcia <martigarcia@iesmontsia.org>
2025-05-03 18:13:38 +0200

    feat: update testing libraries and configuration for improved testing support

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-// vitest.config.ts
 import path from 'path';
 import { defineConfig } from 'vitest/config';
 
@@ -6,10 +5,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'resources/ts'),
+      '@resources': path.resolve(__dirname, 'resources'),
+      '@images': path.resolve(__dirname, 'resources/images'),
+      '@components': path.resolve(__dirname, 'resources/ts/components'),
+      '@utils': path.resolve(__dirname, 'resources/ts/utils'),
     },
   },
   test: {
     globals: true,
+    setupFiles: 'vitest.setup.ts',
     environment: 'jsdom',
     include: [
       'resources/ts/**/*.{test,spec}.{ts,tsx}',
